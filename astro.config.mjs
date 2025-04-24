8396c880c9bf58c0ab29b40ae75c412f7691c757
Gildardo Tello Alvarez <146305840+NotPoncho23@users.noreply.github.com>
2025-04-24 16:04:52 -0600

    change on config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://NotPoncho23.github.io',
-  base: '/portfolio',
   integrations: [tailwind()]
 });
